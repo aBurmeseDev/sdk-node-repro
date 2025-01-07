594b46d9332695d0a9adfa98f04a617b335bb1a5
John Lwin <johnlwin@amazon.com>
2025-01-07 14:10:06 -0800

    add create-stack to import required packages

---
+++ create-stack.mjs
@@ -0,0 +1,22 @@
+import { CloudFormationClient, CreateStackCommand } from '@aws-sdk/client-cloudformation';
+
+const client = new CloudFormationClient({ region: process.env.AWS_REGION });
+
+const params = {
+  StackName: `my-stack-node-${process.version.replace(/\./g, '-')}`,
+  TemplateBody: JSON.stringify({
+    Resources: {
+      MyBucket: {
+        Type: 'AWS::S3::Bucket',
+        Properties: {
+          BucketName: 'sdk-node-repro'
+        }
+      }
+    }
+  }),
+  Capabilities: ['CAPABILITY_NAMED_IAM']
+};
+
+const command = new CreateStackCommand(params);
+const response = await client.send(command);
+console.log(response);
